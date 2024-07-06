02666417e69cf5159e600e6f2202ce30e2d531f7
chuongdinh <dinhvanchuong2k@gmail.com>
2024-07-06 09:30:56 +0700

    test 2

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/user", (req, res) => {
-  return res.status(200).send("Hello world!");
+  return res.status(200).send("Hello world 123!");
 });
 
 app.listen(port, () => {
