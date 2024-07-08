42499d245fbf5e37945b3fc6475d9f43f6726b91
chuongdinh <dinhvanchuong2k@gmail.com>
2024-07-08 11:48:06 +0700

    test commit 4

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server 123");
+  res.send("Welcome to Ducky Project - chuongdev");
 });
 
 app.get("/user", (req, res) => {
